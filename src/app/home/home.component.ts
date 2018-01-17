ad8fa244448fa2061f69fa2af5e06e97cd395c1a
Bharat Jain <bharat.jain325@gmail.com>
2018-01-17 11:59:21 +0000

    To update itemCount on removing item

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -54,6 +54,7 @@ export class HomeComponent implements OnInit {
 
   removeItem(i) {
     this.goals.splice(i, 1);
+    this.itemCount = this.goals.length;
     this._data.changeGoal(this.goals);
   }
 
